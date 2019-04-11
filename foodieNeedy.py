c4771c50f220d220a3c26c58832cbecae139211b
elisabethschiele <elisabethschiele@gmail.com>
2019-04-11 11:07:59 +0200

    more or less elegant solutions...

--- foodieNeedy.py
+++ foodieNeedy.py
@@ -8,10 +8,11 @@ from flask import Flask, jsonify, request
 # create the app. This variable named jh constains our app'
 FOODIE_NEEDY = Flask(__name__)
 
-schools = ['ABC SCHOOL', 'DEF SCHOOL', 'GHI SCHOOL']
-address = ['2B, BALLYGUNGE ROAD,KOLKATA-32', '5F, PARK STREET,KOLKATA-17', '18D,BEHALA ROAD,KOLKATA-56']
-location = ['22.533,88.366', '22.576,88.350', '22.498,88.310']
-time = ['8am-9am', '10am-11am', '7am-8am']
+schools = ['Mahadevi Birla World Academy', ' St.Francis Xavier School', 'Kendriya Vidyalaya Ballygunge']
+address = ['17A  Dargah Road,Park Circus,Kolkata-17', 'GA Road, Purbanchal, Phase1 ,Sector 3,Salt Lake City ,Kolkata-97', 'Military Camp,Ballygunge Circular Road, Ballygunge Kolkata-97']
+location = ['22.540 , 88.370', '22.573 , 88.406', '22.533 , 88.353']
+dropoff = ['9:00am to 11:00am', '7:00 am to 10:30am', '10:00 am to 12:00pm']
+pickup = [' 4:00pm to 5:30pm', '3:00pm to 4:30pm', '5:00pm to 7:30pm']
 
 
 # add routes to our app
@@ -21,16 +22,22 @@ def foodie_needy():
 
 @FOODIE_NEEDY.route('/location')
 def LOCATION():
-    return jsonify(location)
+    resp = jsonify(location)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 @FOODIE_NEEDY.route('/information')
 def INFORMATION():
     twod=[]
     for i in range(len(schools)):
-        oned=[schools[i],address[i],time[i]]
+        oned=[schools[i],address[i],dropoff[i],pickup[i]]
         twod.append(oned)
-    return jsonify(twod)
+
+    resp = jsonify(twod)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp
 '''
 
 @jh.route('/city/add')
--- js/own.js
+++ js/own.js
@@ -1,4 +1,5 @@
 var mymap = L.map('mapid').setView([22.572, 88.363], 13);
+var markers = [];
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,6 +7,31 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZWxpc2FiZXRoc2NoaWVsZSIsImEiOiJjanViaDhycTQwMW02NGFxanozOHl4YnN5In0.hEeCkoBE_0Nj3wosrrEHGQ'
 }).addTo(mymap)
+var marker = L.marker([22.533 , 88.353]).addTo(mymap);
+$.getJSON("http://127.0.0.1:5000/location", function(data) {
 
-var marker = L.marker([22.572, 88.363]).addTo(mymap);
-marker.bindPopup("<b>school name</b><br>Dropoff: <br>Pickup: <br>Adress: ");
+  for (var i = 0; i < data.length-1; i++) {
+    //alert(data[i]);
+    var splitData = data[i].split(",");
+    var lat = parseFloat(splitData[0]); //alert(parseFloat(splitData[0]));
+    var long = parseFloat(splitData[1]); //log.console(splitData[1]);
+
+    var marker = L.marker([lat, long]).addTo(mymap);
+
+    marker.on('click', function (e){
+
+      $.getJSON("http://127.0.0.1:5000/information", function(data1) {
+            console.log(data1)
+            var name = data1[0][0];
+            var adress = data1[i][1];
+            var dropoff = data1[i][2];
+            var pickup = data1[i][3];
+            marker.bindPopup("<b>"+name+"</b><br>Dropoff: "+dropoff+"<br>Pickup: "+pickup+"<br>Adress: "+adress);
+            marker.openPopup();
+    })
+  })
+  }
+})
+
+//var marker = L.marker([22.572, 88.363]).addTo(mymap);
+//marker.bindPopup("<b>school name</b><br>Dropoff: <br>Pickup: <br>Adress: ");
